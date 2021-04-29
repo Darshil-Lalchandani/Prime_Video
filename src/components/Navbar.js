f0b60ee4488249cc3ac169554c87a2c122dba43e
Darshil Lalchandani <ldarshill@gmail.com>
2021-04-30 00:45:28 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/prime.png'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaSistrix, FaUser } from 'react-icons/fa'
 
 const Navbar = () => {
+  const [currentPage, setCurrentPage] = useState('home')
   return (
     <nav className="navbar navbar-expand-sm text-white fixed-top">
       <a className="navbar-brand ml-4 mt-2" href='/'>
@@ -11,22 +12,34 @@ const Navbar = () => {
       <button className="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"><FaBars /></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link text-white" href="#">Home</a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent parent">
+        <ul className="navbar-nav child1">
+          <li className={currentPage === 'home' ? 'active nav-item' : 'nav-item'}>
+            <a className="nav-link text-white" href="#" onClick={() => setCurrentPage('home')}>Home</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">TV Shows</a>
+          <li className={currentPage === 'tv-shows' ? 'active nav-item' : 'nav-item'}>
+            <a className="nav-link text-white" href="#" onClick={() => setCurrentPage('tv-shows')}>TV Shows</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">Movies</a>
+          <li className={currentPage === 'movies' ? 'active nav-item' : 'nav-item'}>
+            <a className="nav-link text-white" href="#" onClick={() => setCurrentPage('movies')}>Movies</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">Kids</a>
+          <li className={currentPage === 'kids' ? 'active nav-item' : 'nav-item'}>
+            <a className="nav-link text-white" href="#" onClick={() => setCurrentPage('kids')}>Kids</a>
           </li>
-      </ul>
-    </div>
+        </ul>
+        <ul className='navbar-right ml-auto child2'>
+        <form className="form-inline">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text"><FaSistrix /></div>
+            </div>
+          <input className="form-control mr-sm-2 mt-4 text-white" type="search" placeholder="Search" aria-label="Search" />
+          </div>
+        <FaUser className="userIcon"/>
+        <p className="userName">Test</p>
+        </form>
+        </ul>
+      </div>
     </nav>
   )
 }
